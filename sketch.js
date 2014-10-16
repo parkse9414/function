3219ea6b404ab987e07adb2acf48a506e7c0f4b8
박세원 <parkse9414@naver.com>
2014-10-16 16:21:10 +0900

    dfr

--- sketch.js
+++ sketch.js
@@ -3,9 +3,24 @@ function setup()
 	createCanvas(800,800)
 }
 
-funtion draw()
+function draw()
 {
 	background(0);
 	fill(255)
-	ellspe(mouseX, mouseY, 100, 100)
+	ellipse(mouseX, mouseY, 100, 100)
+
+    drawDonut1(248, 181, 81, 100,100);
+    drawDonut1(128, 194, 105, 300,100);
+    drawDonut1(126, 206, 244, 400,200);
+    drawDonut1(255, 125, 140, 500,300);
+
+}
+
+function drawDonut1(r, g, b, x, y)
+{
+	//도넛1
+    fill(r, g, b, 255)
+	ellipse(x, y, 200, 200)
+    fill(0)
+	ellipse(x, y, 120, 120)
 }
